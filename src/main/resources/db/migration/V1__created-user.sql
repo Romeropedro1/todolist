f3d215e13e179e00b70161fc846d0df914d1251b
Silvio-Hub <silviocelso.dev@gmail.com>
2023-12-19 11:54:23 -0300

    Dockerfile deploy

---
+++ src/main/resources/db/migration/V1__created-user.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE users (
+--     id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+--     name VARCHAR(50) NOT NULL,
+--     username VARCHAR(50),
+--     password VARCHAR(20),
+--     createdAt TIMESTAMP
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ src/test/java/br/com/celsosilva/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.celsosilva.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
